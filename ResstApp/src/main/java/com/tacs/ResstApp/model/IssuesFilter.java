c1b5b71cbc0dc2c9e3b154fa4719e135f0c4d82f
lu <lucia_roldan@outlook.com>
2019-09-10 19:53:03 -0300

    Agrega clases para los distintos tipos de filtros

---
+++ ResstApp/src/main/java/com/tacs/ResstApp/model/IssuesFilter.java
@@ -0,0 +1,9 @@
+package com.tacs.ResstApp.model;
+
+public class IssuesFilter {
+    private Integer nofissues;
+
+    public IssuesFilter(Integer nofissues) {
+        this.nofissues = nofissues;
+    }
+}
