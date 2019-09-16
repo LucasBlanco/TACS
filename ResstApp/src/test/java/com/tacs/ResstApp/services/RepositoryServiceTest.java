ba23fea3aab7cde1924a7efb952624b9a3d71a69
Cristian Matias Giorda <matiasgiorda27@gmail.com>
2019-09-16 03:02:53 -0300

    Refactor RepositoryServiceTest

--- ResstApp/src/test/java/com/tacs/ResstApp/services/RepositoryServiceTest.java
+++ ResstApp/src/test/java/com/tacs/ResstApp/services/RepositoryServiceTest.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -21,60 +21,49 @@ import com.tacs.ResstApp.services.impl.RepositoryService;
 public class RepositoryServiceTest {
 
 	@InjectMocks
-    RepositoryService repositoryService = new RepositoryService();
+	RepositoryService repositoryService = new RepositoryService();
 	@Mock
 	GitService gitService = new GitService();
 
-    private List<Repository> repositories;
-	private Repository repo1 = new Repository(1L, "TACS");
-	private Repository repo2 = new Repository(2L, "TADP");
-    private LocalDate referenceTime = LocalDate.now();
+	private List<Repository> repositories;
+	private Repository repository1 = new Repository(1L, "TACS");
+	private Repository repository2 = new Repository(2L, "TADP");
+	private LocalDate referenceTime = LocalDate.now();
 
-    @BeforeEach
-    public void before() throws Exception {
-        repo1.setRegistrationDate(referenceTime.minusDays(2));
-        repo2.setRegistrationDate(referenceTime.plusDays(2));
-        this.repositories = new ArrayList<>(Arrays.asList(repo1, repo2));
+	@BeforeEach
+	public void before() throws Exception {
+		repository1.setRegistrationDate(referenceTime.minusDays(2));
+		repository2.setRegistrationDate(referenceTime.plusDays(2));
+		this.repositories = Arrays.asList(repository1, repository2);
 		Mockito.when(gitService.getRepositories()).thenReturn(this.repositories);
+	}
+
+	@Test
+    public void repositoryServiceDoesNotReturnAnyRepositoriesInOpenIntervalBetweenRepositories() throws Exception {
+        List<Repository> repositoriesInOpenInterval = 
+        		repositoryService.getRepositoriesBetween(referenceTime.minusDays(1),referenceTime.plusDays(1));
+		Assertions.assertThat(repositoriesInOpenInterval).isEmpty();
     }
 
-    @Test
-    public void repositoriesBetween1() throws Exception {
-        Assertions.assertTrue(
-        		repositoryService.getRepositoriesBetween(
-                        referenceTime.minusDays(1),
-                        referenceTime.plusDays(1)
-                ).isEmpty()
-		);
-    }
+	@Test
+	public void repositoryServiceReturnsRepository1InIntervalIncludingOnlyThatRepository() throws Exception {
+		List<Repository> repositoriesInIntervalContainingRepository1 = 
+				repositoryService.getRepositoriesBetween(referenceTime.minusDays(3), referenceTime.plusDays(1));
+		Assertions.assertThat(repositoriesInIntervalContainingRepository1).containsExactly(repository1);
+	}
 
-    @Test
-    public void repositoriesBetween2() throws Exception {
-        Assertions.assertEquals(
-        		repo1,
-				repositoryService.getRepositoriesBetween(
-						referenceTime.minusDays(3),
-						referenceTime.plusDays(1)
-				).get(0));
-    }
+	@Test
+	public void repositoryServiceReturnsBothRepositoriesInIntervalIncludingThem() throws Exception {
+		List<Repository> repositoriesInClosedInterval = 
+				repositoryService.getRepositoriesBetween(referenceTime.minusDays(3), referenceTime.plusDays(3));
+		Assertions.assertThat(repositoriesInClosedInterval).containsExactly(repository1, repository2);
+	}
 
-    @Test
-    public void repositoriesBetween3() throws Exception {
-        Assertions.assertEquals(
-        		2,
-				repositoryService.getRepositoriesBetween(
-						referenceTime.minusDays(3),
-						referenceTime.plusDays(3)
-				).size());
-    }
-
-    @Test
-    public void repositoriesBetween4() throws Exception {
-        Assertions.assertTrue(
-        		repositoryService.getRepositoriesBetween(
-        				referenceTime.plusDays(5),
-						referenceTime.plusDays(7)
-				).isEmpty());
-    }
+	@Test
+	public void repositoryServiceDoesNotReturnAnyRepositoriesInExternalInterval() throws Exception {
+		List<Repository> repositoriesInExternalInterval = 
+				repositoryService.getRepositoriesBetween(referenceTime.plusDays(5), referenceTime.plusDays(7));
+		Assertions.assertThat(repositoriesInExternalInterval).isEmpty();
+	}
 
 }
