173c5ab6bbd4260bbd2b544736a102aace6f3d1e
Juan <juan.constenla@gmail.com>
2019-09-28 14:28:47 -0300

    Agrego filtro de urls de administrador

--- ResstApp/src/main/java/com/tacs/ResstApp/AppConfig.java
+++ ResstApp/src/main/java/com/tacs/ResstApp/AppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.tacs.ResstApp.filters.AdminFilter;
 import com.tacs.ResstApp.filters.TokenFilter;
 
 @Configuration
@@ -15,23 +16,36 @@ import com.tacs.ResstApp.filters.TokenFilter;
 public class AppConfig {
 	@Value("${loggedUrls}")
 	private String[] loggedUrls;
+	@Value("${adminUrls}")
+	private String[] adminUrls;
 	
-	@Bean
+	@Bean(name="TokenFilter")
 	public FilterRegistrationBean<TokenFilter> filterRegistrationBean() {
 		FilterRegistrationBean<TokenFilter> registrationBean = new FilterRegistrationBean<TokenFilter>();
 		TokenFilter tokenFilter = new TokenFilter();
 
 		registrationBean.setFilter(tokenFilter);
 		if (loggedUrls!=null) {
-			
 			registrationBean.addUrlPatterns(loggedUrls);
 		}
-			
-		//registrationBean.addUrlPatterns("/user/*");//TODO: Asignar a que urls se hay que autenticar
 		registrationBean.setOrder(1);
 		return registrationBean;
 	}
+
 	
+	@Bean(name="AdminFilter")
+	public FilterRegistrationBean<AdminFilter> adminFilterRegistrationBean() {
+		FilterRegistrationBean<AdminFilter> registrationBean = new FilterRegistrationBean<AdminFilter>();
+		AdminFilter adminFilter = new AdminFilter();
+
+		registrationBean.setFilter(adminFilter);
+		if (adminUrls!=null) {
+			registrationBean.addUrlPatterns(adminUrls);
+			
+		}
+		registrationBean.setOrder(2);
+		return registrationBean;
+	}
 	@Bean
     public CacheManager cacheManager() {
         return new ConcurrentMapCacheManager("repos");
