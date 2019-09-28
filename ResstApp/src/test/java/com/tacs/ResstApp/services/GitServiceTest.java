d943a4b01a4085f0b45b499d3ac721944a5ca091
Cristian Matias Giorda <matiasgiorda27@gmail.com>
2019-09-27 22:26:15 -0300

    Fix gitServiceTest

--- ResstApp/src/test/java/com/tacs/ResstApp/services/GitServiceTest.java
+++ ResstApp/src/test/java/com/tacs/ResstApp/services/GitServiceTest.java
@@ -1,12 +1,13 @@
 package com.tacs.ResstApp.services;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.tacs.ResstApp.model.Repository;
 import com.tacs.ResstApp.services.impl.GitService;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
 
 @SpringBootTest
 public class GitServiceTest {
@@ -23,7 +24,11 @@ public class GitServiceTest {
 				"    \"language\": null,\r\n" +  
 				"    \"forks_count\": 5,\r\n" + 
 				"    \"open_issues_count\": 2,\r\n" + 
-				"    \"subscribers_count\": 1\r\n" + 
+				"    \"owner\": {\r\n" +
+				"    \"login\": \"owner\"\r\n" +
+				"    },\r\n" +
+				"    \"size\": 800,\r\n" +
+				"    \"subscribers_count\": 1\r\n" +
 				"}";
 		
 		Repository repo = gitService.parseRepository(json);
@@ -35,5 +40,7 @@ public class GitServiceTest {
 		Assertions.assertEquals(5, repo.getNofForks());
 		Assertions.assertEquals(0, repo.getStars());
 		Assertions.assertEquals(2, repo.getTotalIssues());
+		Assertions.assertEquals(800, repo.getSize());
+		Assertions.assertEquals("owner", repo.getOwner());
 	}
 }
