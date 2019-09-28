2360e068290852ed1b52deb77b8a1ecbed93a7cc
Cristian Matias Giorda <matiasgiorda27@gmail.com>
2019-09-28 20:19:08 -0300

    Fixes bd y remuevo lógica en interceptor

--- ResstApp/src/main/resources/data.sql
+++ ResstApp/src/main/resources/data.sql
@@ -7,13 +7,13 @@ INSERT INTO tacs.user (username, password, admin) VALUES
 ('admin2','1234',true);
 
 TRUNCATE TABLE tacs.repository;
-INSERT INTO tacs.repository(id, favs, main_language, nof_forks, score, total_commits, total_issues, owner, name, registration_date) VALUES 
-(205392106, 5, "C", 6, 1, 5, 5, "LucasBlanco", "TACS", DATE('2019-09-22')),
-(161207084, 5, "C", 6, 1, 5, 5, "LuciaRoldan", "tp-tadp", DATE('2019-09-20')),
-(153519881, 1, "C", 0, 0, 0, 0, "rociochipian", "gdd_mate_lavado", DATE('2019-09-21')),
-(107976932, 2, "JAVA", 2, 5, 10, 4, "matigiorda", "TaTeTiWollok", DATE('2019-09-19')),
-(141071043, 10, "JAVA", 3, 1, 3, 32, "lucasmcenturion", "Fail-System", DATE('2019-09-22')),
-(138937247, 11, "RUBY", 22, 3, 13, 3, "Juam", "Simplex-Solucion", DATE('2019-09-22'));
+INSERT INTO tacs.repository(id, favs, main_language, nof_forks, score, total_commits, total_issues, owner, name, registration_date, size) VALUES 
+(205392106, 5, "C", 6, 1, 5, 5, "LucasBlanco", "TACS", DATE('2019-09-22'), 100),
+(161207084, 5, "C", 6, 1, 5, 5, "LuciaRoldan", "tp-tadp", DATE('2019-09-20'), 100),
+(153519881, 1, "C", 0, 0, 0, 0, "rociochipian", "gdd_mate_lavado", DATE('2019-09-21'), 100),
+(107976932, 2, "JAVA", 2, 5, 10, 4, "matigiorda", "TaTeTiWollok", DATE('2019-09-19'), 100),
+(141071043, 10, "JAVA", 3, 1, 3, 32, "lucasmcenturion", "Fail-System", DATE('2019-09-22'), 100),
+(138937247, 11, "RUBY", 22, 3, 13, 3, "Juam", "Simplex-Solucion", DATE('2019-09-22'), 100);
 
 TRUNCATE TABLE tacs.user_favourites;
 INSERT INTO tacs.user_favourites(user_id, favourites_id) VALUES
