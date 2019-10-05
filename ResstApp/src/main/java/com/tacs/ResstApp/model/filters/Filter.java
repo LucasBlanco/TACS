db6fcedf645f9db280f95e41bb82c9a53fa96f11
tptacs <rociochipian@gmail.com>
2019-10-05 12:17:46 -0300

    starting el filter

--- ResstApp/src/main/java/com/tacs/ResstApp/model/filters/Filter.java
+++ ResstApp/src/main/java/com/tacs/ResstApp/model/filters/Filter.java
@@ -1,6 +1,6 @@
 package com.tacs.ResstApp.model.filters;
 
-public abstract class Filter {
+public abstract class Filter { //voy a tener un property filter que no va a tener un value.
 	public String getQueryProperty() {
 		return getPropertyName() + ":" + getValue();
 	}
