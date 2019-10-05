a56a02c15544d96add920b20f4cb3d9fad0791f3
Cristian Matias Giorda <matiasgiorda27@gmail.com>
2019-10-05 19:30:18 -0300

    Se remueven datos referidos a commits

--- ResstApp/src/main/resources/data.sql
+++ ResstApp/src/main/resources/data.sql
@@ -9,13 +9,13 @@ INSERT INTO tacs.user (username, password, admin) VALUES
 ('user','WnxBw10jL2M=dXNlcg==', false);
 
 TRUNCATE TABLE tacs.repository;
-INSERT INTO tacs.repository(id, favs, main_language, nof_forks, score, total_commits, total_issues, owner, name, registration_date, size, stars) VALUES 
-(205392106, 0, "C", 6, 1, 5, 5, "LucasBlanco", "TACS", DATE('2019-09-22'), 100, 1),
-(161207084, 0, "C", 6, 1, 5, 5, "LuciaRoldan", "tp-tadp", DATE('2019-09-20'), 100, 2),
-(153519881, 0, "C", 0, 0, 0, 0, "rociochipian", "gdd_mate_lavado", DATE('2019-09-21'), 100, 3),
-(107976932, 0, "JAVA", 2, 5, 10, 4, "matigiorda", "TaTeTiWollok", DATE('2019-09-19'), 100, 4),
-(141071043, 0, "JAVA", 3, 1, 3, 32, "lucasmcenturion", "Fail-System", DATE('2019-09-22'), 100, 5),
-(138937247, 0, "RUBY", 22, 3, 13, 3, "Juam", "Simplex-Solucion", DATE('2019-09-22'), 100, 6);
+INSERT INTO tacs.repository(id, favs, main_language, nof_forks, score, total_issues, owner, name, registration_date, size, stars) VALUES 
+(205392106, 0, "C", 6, 1, 5, "LucasBlanco", "TACS", DATE('2019-09-22'), 100, 1),
+(161207084, 0, "C", 6, 1, 5, "LuciaRoldan", "tp-tadp", DATE('2019-09-20'), 100, 2),
+(153519881, 0, "C", 0, 0, 0, "rociochipian", "gdd_mate_lavado", DATE('2019-09-21'), 100, 3),
+(107976932, 0, "JAVA", 2, 5, 4, "matigiorda", "TaTeTiWollok", DATE('2019-09-19'), 100, 4),
+(141071043, 0, "JAVA", 3, 1, 32, "lucasmcenturion", "Fail-System", DATE('2019-09-22'), 100, 5),
+(138937247, 0, "RUBY", 22, 3, 3, "Juam", "Simplex-Solucion", DATE('2019-09-22'), 100, 6);
 
 TRUNCATE TABLE tacs.user_favourites;
 INSERT INTO tacs.user_favourites(user_id, favourites_id) VALUES
